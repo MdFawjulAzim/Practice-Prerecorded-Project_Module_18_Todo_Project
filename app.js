6865265e13a987436e4417948e6a6663daf9dcf3
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2024-11-19 01:23:56 +0600

    app.js add

---
+++ app.js
@@ -0,0 +1,40 @@
+//Basic
+
+const express = require('express')
+const router = require('./routes/api.js');
+const app = express();
+const bodyParser = require('body-parser');
+
+//security Middleware Lib Import
+
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp =require('hpp');
+const cors = require('cors');
+
+//Database Lib Import
+
+const mongoose = require('mongoose');
+
+
+//Security Middleware Implement 
+
+app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+
+//Body Parser Implement
+
+app.use(bodyParser.json());
+
+//Request rate limit
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
